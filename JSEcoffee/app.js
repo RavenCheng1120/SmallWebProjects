4f5d4c253d0213eb2b6e90eb807bf2962415720c
RavenCheng1120 <ravencheng66@gmail.com>
2020-05-06 13:39:10 +0800

    second scroll page

--- JSEcoffee/app.js
+++ JSEcoffee/app.js
@@ -12,3 +12,16 @@ tl.fromTo(hero, 1.4, {height:'0%'}, {height:'80%', ease: "power3.inOut"})
   .fromTo(logo, 0.6, {opacity: 0, x: -30}, {opacity: 1, x:0}, "-=1")
   .fromTo(headline, 0.6, {opacity: 0}, {opacity: 1}, "-=0.8")
   .fromTo(jackThumbnail, 1, {opacity: 0, y: 30, scaleY: -1}, {opacity: 1, y:0, scaleY: -1}, "-=1");
+
+function scrollAppear(){
+  const coffeeArticle = document.querySelector('.coffee-article');
+  const articlePosition = coffeeArticle.getBoundingClientRect().top;
+  const screenposition = window.innerHeight;
+
+  if(articlePosition < screenposition/2){
+    coffeeArticle.classList.add('coffee-article-appear');
+  }
+
+}
+
+document.addEventListener('scroll',scrollAppear);
